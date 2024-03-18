5e23f2aa1116e528bb43199286dad52f91327c61
Brendan Musick <bmuze1@gmail.com>
2024-03-18 14:53:15 -0600

    Set up morgan logger middleware.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 ```
 - See this document to see how to transpile to javascript before use in production code
+
+# Implemented logger middleware
+https://expressjs.com/en/resources/middleware/morgan.html
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const fs = require('fs');
+var path = require('path');
+
+// Add built in middleware to deconstruct url parameters content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // Middleware to use json body data
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
